a28dd2a1cad3d05b3a0469711fdf223bc709e1a3
MichaTrojek <MichalTrojek1@gmail.com>
2021-02-27 22:59:09 +0100

    added mock data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
+  ];
 
   constructor() {}
 
