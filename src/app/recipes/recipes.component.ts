f917990eada75d54417fb9f517d08481e82f19d3
MichaTrojek <MichalTrojek1@gmail.com>
2021-03-07 00:11:47 +0100

    added cross component communication

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -11,11 +11,11 @@ import { RecipeService } from './service/recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
-
-  setSelectedRecipe(selectedRecipe: Recipe) {
-    this.selectedRecipe = selectedRecipe;
+  ngOnInit(): void {
+    this.recipeService.recipeWasSelected.subscribe((recipe: Recipe) => {
+      this.selectedRecipe = recipe;
+    });
   }
 }
