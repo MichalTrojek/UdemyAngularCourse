d7c755275579b65e9bb38457cc1ee901cf349011
MichaTrojek <MichalTrojek1@gmail.com>
2021-03-10 23:21:13 +0100

    replaced eventEmitter with Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  loadedFeature: string = 'recipes';
-  title: 'test';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
-}
+export class AppComponent {}
