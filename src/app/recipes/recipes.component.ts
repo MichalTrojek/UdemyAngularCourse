c31b92b386c4c3e859e40fda1fab5f693cb75bcd
MichaTrojek <MichalTrojek1@gmail.com>
2021-03-11 22:03:17 +0100

    fixed exception

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
+
 import { RecipeService } from './service/recipe.service';
 
 @Component({
@@ -10,7 +10,5 @@ import { RecipeService } from './service/recipe.service';
 })
 export class RecipesComponent implements OnInit {
   constructor() {}
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+  ngOnInit(): void {}
 }
